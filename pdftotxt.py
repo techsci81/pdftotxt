b53e17727c8d3df02b5264ca9a81734946d43107
techsci81 <57443695+techsci81@users.noreply.github.com>
2026-07-17 16:18:19 +0530

    Clarify file path in pdftotxt.py comments
    Added comments to clarify file path usage.

--- pdftotxt.py
+++ pdftotxt.py
@@ -1,9 +1,9 @@
 # USEFUL WHEN ONLY ONE FILE NEED TO BE CONVERTED TO TXT
 import PyPDF2
-
+# REPLACE "rootdrive:\folder\subfolder\filename.pdf" with your path & file name.
 pdf_file = open(r"rootdrive:\folder\subfolder\filename.pdf", "rb")
 reader = PyPDF2.PdfReader(pdf_file)
-
+# REPLACE "rootdrive:\folder\subfolder\filename.pdf" with your path & file name.
 with open(r"rootdrive:\folder\subfolder\filename.txt", "w", encoding="utf-8") as txt_file:
     for page in reader.pages:
         txt_file.write(page.extract_text() + "\n")
